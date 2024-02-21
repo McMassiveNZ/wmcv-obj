7fb952d73670ff6ac143b547463842f4198abe47
William McVicar <12842622+McMassiveNZ@users.noreply.github.com>
2024-02-21 19:42:45 +0100

    Initial commit

---
+++ cmake/gtest.cmake
@@ -0,0 +1,17 @@
+include_guard()
+
+CPMAddPackage(
+	NAME googletest
+	GITHUB_REPOSITORY google/googletest
+	GIT_TAG v1.13.0
+	VERSION 1.13.0
+	OPTIONS
+	"INSTALL_GTEST OFF"
+	"gtest_force_shared_crt ON"
+)
+set_property(TARGET 
+	gtest 
+	gtest_main 
+	gmock
+	gmock_main
+	PROPERTY FOLDER third_party/GoogleTest)
